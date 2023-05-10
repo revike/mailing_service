bd4d5dbcd123e766ba3b57ebf505acce91392209
revike <revike@ya.ru>
2023-05-11 01:15:23 +0300

    crud for client

--- client/models.py
+++ client/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from base.models import NULLABLE
 from client.services import get_timezones
 from client.validators import validate_phone
 
@@ -8,8 +9,9 @@ class Client(models.Model):
     """Model client"""
     TIMEZONES = get_timezones()
     phone = models.CharField(max_length=16, validators=[validate_phone], unique=True, verbose_name='phone')
-    tag = models.CharField(max_length=64, verbose_name='tag')
-    location = models.CharField(max_length=128, choices=TIMEZONES, verbose_name='location')
+    tag = models.CharField(max_length=64, **NULLABLE, verbose_name='tag')
+    location = models.CharField(max_length=128, **NULLABLE, choices=TIMEZONES, verbose_name='location')
+    is_active = models.BooleanField(default=True, verbose_name='is_active')
 
     class Meta:
         verbose_name = 'client'
--- client/views.py
+++ client/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from client.models import Client
+from client.serializers import ClientSerializer
+from client.services import get_timezones
+
+
+class ClientCreateApiView(generics.CreateAPIView):
+    """Create client"""
+    serializer_class = ClientSerializer
+
+
+class ClientUpdateApiView(generics.UpdateAPIView):
+    """Update client"""
+    serializer_class = ClientSerializer
+    queryset = Client.objects.filter(is_active=True)
+
+
+class ClientDeleteApiView(generics.DestroyAPIView):
+    """Delete client"""
+    queryset = Client.objects.filter(is_active=True)
+
+    def perform_destroy(self, instance):
+        if instance.is_active:
+            instance.is_active = False
+            instance.save()
+
+
+class LocationListApiView(APIView):
+    """Get list locations"""
+    location_params = openapi.Parameter(
+        'location', in_=openapi.IN_QUERY, description='location', type=openapi.TYPE_STRING
+    )
+
+    @swagger_auto_schema(manual_parameters=[location_params])
+    def get(self, request):
+        search = request.GET.get('location', None)
+        locations = get_timezones()
+        data = [location[0] for location in locations]
+        if search:
+            data = [location for location in data if search.lower() in location.lower()]
+        return Response(data, status=status.HTTP_200_OK)
